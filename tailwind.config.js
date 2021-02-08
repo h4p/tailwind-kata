44ba43119cd86fb7b743b958ba46a4429e5d0040
twe <tobias.wessels@ongoingmedia.de>
2021-02-08 17:42:29 +0100

    Remove media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./layouts/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.lightBlue,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
+    },
     extend: {},
   },
   variants: {
